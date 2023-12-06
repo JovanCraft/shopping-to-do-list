301c54281f2733b7d69df5e838467e65be381ab6
jovan <jovancraft@gmail.com>
2023-12-06 11:10:29 -0600

    trying out how to situate the input field and add button

--- src/components/ListForm.js
+++ src/components/ListForm.js
@@ -24,7 +24,7 @@ class ListForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.submitForm}>
+      <form className='input' onSubmit={this.submitForm}>
         {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
         <input type="text" name="item" value={this.state.item} onChange={this.handleChanges}/>
         <button>Add</button>
